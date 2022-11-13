af1629de5619242cd2f2bfa341645cb413b34354
nick <me@nickc.io>
2022-11-12 19:43:24 -0500

    fix: Add better commander logging

--- command/main.ts
+++ command/main.ts
@@ -3,7 +3,15 @@ import { CommandFactory } from 'nest-commander';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  await CommandFactory.run(AppModule);
+  await CommandFactory.run(AppModule, ['warn', 'error']);
 }
 
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log('command bootstraped');
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.error('failed to start command server', error);
+    process.exit(1);
+  });
