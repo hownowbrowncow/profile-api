0cace8fd980319fa7b8379b3b4f36930ee271a9a
nick <me@nickc.io>
2022-11-12 19:43:24 -0500

    feat: Configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Profile API')
+    .setDescription('Profile API Swagger')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
 
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
