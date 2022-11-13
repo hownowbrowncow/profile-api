20525d75a38eac9fff49d2eadde7e426e58aa8ac
nick <me@nickc.io>
2022-11-12 19:43:24 -0500

    feat: Configure db connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const typeormConfig = TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'docker',
+  database: 'profile_api',
+  entities: [],
+  synchronize: false,
+});
+
 @Module({
-  imports: [],
+  imports: [typeormConfig],
   controllers: [AppController],
   providers: [AppService],
 })
