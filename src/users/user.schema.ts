7cf2ef968e629c22578ecafe89b08c93fc3ecf20
nick <me@nickc.io>
2022-11-12 19:43:24 -0500

    feat: Update user schema

--- src/users/user.schema.ts
+++ src/users/user.schema.ts
@@ -1,16 +1,19 @@
-import { EntitySchema } from 'typeorm';
+import { EntitySchema, EntitySchemaColumnOptions } from 'typeorm';
 
 export interface UserType {
   id: string;
   name: string;
+  email: string;
   password: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
 
 export const UserSchema = new EntitySchema<UserType>({
   name: 'User',
   columns: {
     id: {
-      type: String,
+      type: 'uuid',
       primary: true,
       generated: 'uuid',
     },
@@ -18,8 +21,22 @@ export const UserSchema = new EntitySchema<UserType>({
       type: String,
       unique: true,
     },
+    email: {
+      type: String,
+      unique: true,
+    },
     password: {
       type: String,
     },
+    createdAt: {
+      name: 'created_at',
+      type: 'timestamp with time zone',
+      createDate: true,
+    } as EntitySchemaColumnOptions,
+    updatedAt: {
+      name: 'updated_at',
+      type: 'timestamp with time zone',
+      updateDate: true,
+    } as EntitySchemaColumnOptions,
   },
 });
