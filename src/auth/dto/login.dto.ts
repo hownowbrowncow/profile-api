d850c9105a8a33ddcc56e8a3877590a9ba0d465b
nick <me@nickc.io>
2022-11-12 19:43:25 -0500

    fix: Update login DTO

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDto {
-  @ApiProperty({ type: String })
+  @ApiProperty({ type: String, example: 'admin@localhost' })
   username: string;
 
-  @ApiProperty({ type: String })
+  @ApiProperty({ type: String, example: 'password' })
   password: string;
 }
