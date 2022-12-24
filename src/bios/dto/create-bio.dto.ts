104f38eb3729d91cdf79ba14f985807b72ac6884
nick <me@nickc.io>
2022-12-24 17:31:55 -0500

    fix: Update bio DTOs

--- src/bios/dto/create-bio.dto.ts
+++ src/bios/dto/create-bio.dto.ts
@@ -1,6 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateBioDto {
-  displayName: string;
-  info: string;
-  title: string;
-  picture: string;
+  @ApiProperty({ type: String, example: 'Admin User' })
+  readonly displayName: string;
+  @ApiProperty({ type: String, example: 'Some info about stuff yano' })
+  readonly info: string;
+  @ApiProperty({ type: String, example: 'Master of nothing' })
+  readonly title: string;
+  @ApiProperty({ type: String, example: 'https://picsum.photos/id/0/400/500' })
+  readonly picture: string;
 }
--- src/bios/dto/update-bio.dto.ts
+++ src/bios/dto/update-bio.dto.ts
@@ -1,4 +1,14 @@
-import { PartialType } from '@nestjs/swagger';
+import { PartialType, ApiProperty } from '@nestjs/swagger';
+
 import { CreateBioDto } from './create-bio.dto';
 
-export class UpdateBioDto extends PartialType(CreateBioDto) {}
+export class UpdateBioDto extends PartialType(CreateBioDto) {
+  @ApiProperty({ type: String, example: 'Admin User' })
+  readonly displayName: string;
+  @ApiProperty({ type: String, example: 'Some info about stuff yano' })
+  readonly info: string;
+  @ApiProperty({ type: String, example: 'Master of nothing' })
+  readonly title: string;
+  @ApiProperty({ type: String, example: 'https://picsum.photos/id/0/400/500' })
+  readonly picture: string;
+}
