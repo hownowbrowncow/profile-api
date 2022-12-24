e3a79a15d8a13e3481e029bcb444e5ca1f6f86c3
nick <me@nickc.io>
2022-12-24 15:35:29 -0500

    fix: Register createBio command

--- command/app.module.ts
+++ command/app.module.ts
@@ -3,14 +3,17 @@ import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 import { TestCommand } from './test/test.command';
 import { CreateUserCommand } from './create/user.command';
+import { CreateBioCommand } from './create/bio.command';
 import { UsersModule } from '../src/users/users.module';
+import { BiosModule } from '../src/bios/bios.module';
 import { defaultConfig } from '../src/database/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(defaultConfig as TypeOrmModuleOptions),
     UsersModule,
+    BiosModule,
   ],
-  providers: [TestCommand, CreateUserCommand],
+  providers: [TestCommand, CreateUserCommand, CreateBioCommand],
 })
 export class AppModule {}
